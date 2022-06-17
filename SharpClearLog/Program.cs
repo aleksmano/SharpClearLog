daa6f9c2e3b2a323c3a6e6b04bc40d018a8e6d7e
aleksmano <aleksmano@yandex.ru>
2022-06-17 16:21:59 +0300

    init repo

---
+++ SharpClearLog/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Diagnostics;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var eventLog = new EventLog("Security", System.Environment.MachineName);
+        eventLog.Clear();
+
+    }
+}
